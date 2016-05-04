59f1aea8ee20049af40b90bd73d7ff066ee7ed77
MohamedJainul <firdavus@live.com>
2016-05-04 07:44:00 +0530

    Create Factorial.java
    Factorial Program in simple coding

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+// Simple code for Factorial programs in java 
+
+
+public class HelloWorld{
+
+     public static void main(String []args){
+    
+            int sum=1,n=6;
+        for(int i=1;i<=n;i++){
+            sum=sum*i;
+        }
+            System.out.println(" "+n+" Factorial is :"+sum);
+        }
+ 
+     }
